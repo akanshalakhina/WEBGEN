afefccfeb70c6e536e21ed403ba1aa5f43a30295
Akansha Lakhina <akanshalakhina23@gmail.com>
2026-02-25 23:10:41 +0530

    Update server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {BrowserRouter,Route, Routes} from "react-router-dom"
 import Home from './pages/home'
+import useGetCurrentUser from './hooks/useGetCurrentUser'
 export const serverUrl="http://localhost:8000"
 function App() {
+  useGetCurrentUser() //to fetch current user data when app component mounts
   return (
     <BrowserRouter>
     <Routes>
---
+++ client/src/hooks/useGetCurrentUser.jsx
@@ -0,0 +1,19 @@
+import { get } from 'mongoose'
+import React, { useEffect } from 'react'
+
+function useGetCurrentUser() {
+  useEffect(()=> {
+    const getCurrentUser=async()=>{
+        try{
+            const result = await axios.get(`${serverUrl}/api/auth/me`,{withCredentials:true})
+            console.log(result)
+        }catch(error){
+            console.error("Error fetching current user:", error)
+        }
+    }
+        getCurrentUser()
+    },[])
+
+}
+
+export default useGetCurrentUser
