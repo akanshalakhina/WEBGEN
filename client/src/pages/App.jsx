838f44536575763a3e2dcc636e0f6010581893fd
Akansha Lakhina <akanshalakhina23@gmail.com>
2026-02-26 22:09:10 +0530

    Created Website Model

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter,Route, Routes} from "react-router-dom"
-import Home from './pages/home'
-import useGetCurrentUser from './hooks/useGetCurrentUser'
+import Home from './home'
+import useGetCurrentUser from '../hooks/useGetCurrentUser'
 export const serverUrl="http://localhost:8000"
 function App() {
   useGetCurrentUser() //to fetch current user data when app component mounts
